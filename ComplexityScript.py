146ab91da384263c60570d8d9912c8d41d0fb034
Gerald <108371085+Ger0sty@users.noreply.github.com>
2023-01-10 19:26:50 -0600

    Added break sections but with broken functionality
    Attempted to implement a break section checker and fix the stack and slider checker but I'm getting errors I am not sure how to resolve

--- ComplexityScript.py
+++ ComplexityScript.py
@@ -5,7 +5,7 @@ import math
 def get_pythagoras(x, y):
     return math.sqrt(x ** 2 + y ** 2)
 
-with open("YourFilePath") as json_data:
+with open("Your Diff File Name") as json_data:
     # add your path above 
     data = json.load(json_data)
 full_dataset = pd.DataFrame(data['_notes'])
@@ -18,23 +18,31 @@ left['_xMovement'] = left.loc[:, ['_xCenter']].diff().fillna(0)
 left['_yMovement'] = left.loc[:, ['_yCenter']].diff().fillna(0)
 left['_totMovement'] = left.apply(lambda x: get_pythagoras(x['_xMovement'], x['_yMovement']), axis=1).fillna(0) 
 left['_angleChange'] = left.apply(lambda x: math.atan(x['_yMovement']/x['_xMovement']), axis=1)
+left['_timeChange'] = left.loc[:, ['_time']].diff().fillna(0)
 
 right['_xMovement'] = right.loc[:, ['_xCenter']].diff().fillna(0)
 right['_yMovement'] = right.loc[:, ['_yCenter']].diff().fillna(0)
 right['_totMovement'] = right.apply(lambda x: get_pythagoras(x['_xMovement'], x['_yMovement']), axis=1).fillna(0)
 right['_angleChange'] = right.apply(lambda x: math.atan(x['_yMovement']/x['_xMovement']), axis=1)
+right['_timeChange'] = left.loc[:, ['_time']].diff().fillna(0)
 
 average_angle = (left['_angleChange'].mean() + right['_angleChange'].mean()) / 2
-time = (full_dataset['_time'].max() - full_dataset['_time'].min()) * bpm / 60
-bpm = #insert bpm of track
+time = 0
+for i in left:
+    if (left.loc[i, '_timeChange'] > 5):
+        time += left.loc[i, '_timeChange']
+for i in right:
+    if (right.loc[i, '_timeChange'] > 5):
+        time += right.loc[i, '_timeChange']
+
 
 total_distance = 0
-count = 2
-number_blocks = full_dataset.count()
-while (count < number_blocks):
-    if full_dataset['_totMovement'] > .5:
-        total_distance = total_distance + (left['_totMovement'] + right['_totMovement']) / 2
-
+for i in left:
+    if (left.loc[i, '_totMovement'] > 0.54):
+        total_distance += left.loc[i, '_totMovement']
+for i in right:
+    if (right.loc[i, '_totMovement'] > 0.54):
+        total_distance += right.loc[i, '_totMovement']
 
 
 average_speed = total_distance/time
